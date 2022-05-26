fca983dcfd8c8052bf85bb42d798162155b91737
Gerson Velasco <gvelassco@gmail.com>
2022-05-26 18:05:07 -0500

    account

--- backend/App/views/principal_all.php
+++ backend/App/views/principal_all.php
@@ -37,7 +37,7 @@
                                 </svg>
                             </a>
                         </li>
-                        <li class="breadcrumb-item text-sm"><a class="opacity-10 text-dark" href="javascript:;">Regresar al Inicio</a></li>
+                        <li class="breadcrumb-item text-sm"><a class="opacity-10 text-dark" href="javascript:;"></a></li>
                     </ol>
                 </nav>
 
