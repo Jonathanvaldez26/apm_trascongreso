7cf437ec3132cf35e326851c0f31e3c2d01dcdfb
Gerson26 <gvelassco@gmail.com>
2022-06-21 00:07:08 -0500

    boton regresar

--- backend/App/views/video_all_congreso_traduccion.php
+++ backend/App/views/video_all_congreso_traduccion.php
@@ -83,6 +83,7 @@
                         </div>
 
                         <div class="col-sm-auto ms-sm-auto mt-sm-0 mt-3 d-flex">
+                            <a href="/Congreso/" class="btn" style="color:#000; background-color: orange; margin-right: 5px;">Regresar</a>
                             <a href="/Congreso/Video/<?=$clave?>" class="btn" style="color:#000; background-color: orange;">Translation</a>
                         </div>
                     </div>
