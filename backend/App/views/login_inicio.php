625ab0543c7087f2f576d6190c777e72f12cb097
Your Name <jonathan.valdez@grupolahe.com>
2022-06-20 21:04:26 -0500

    Hola

--- backend/App/views/login_inicio.php
+++ backend/App/views/login_inicio.php
@@ -6,11 +6,11 @@ echo $header;
 <!DOCTYPE html>
 <html lang="es">
 <head>
-    <title>Congreso SMNP 2022</title>
+    <title>VI Congreso Mundial de Patología Dual</title>
 
 </head>
 <body>
-<iframe style="position:absolute; top:0; left:0; width:100%; height:100%;" src="https://360.congresoneuropediatria.com/" name="SMNP" width="100%" height="100%" frameborder="0" allowfullscreen="true" allow="fullscreen; accelerometer; gyroscope; magnetometer; vr"></iframe>
+<iframe style="position:absolute; top:0; left:0; width:100%; height:100%;" src="https://360es.dualdisorderswaddmexico2022.com/" name="APM" width="100%" height="100%" frameborder="0" allowfullscreen="true" allow="fullscreen; accelerometer; gyroscope; magnetometer; vr"></iframe>
 </body>
 </html>
 
